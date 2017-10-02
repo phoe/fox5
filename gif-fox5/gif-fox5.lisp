e16b86181cbe3325ca5ef6281041125fe6328c88
Michal Herda <michal.herda@ericsson.com>
2017-10-02 17:11:20 +0200

    gif-fox5: begin implementing all the various edit types for choice

--- gif-fox5/gif-fox5.lisp
+++ gif-fox5/gif-fox5.lisp
@@ -19,20 +19,34 @@
 
 (in-package :fox5/gif-fox5)
 
+;;; TODO add ability to convert non-portrait GIFs
+;;; TODO add modifiable conversion types
+;;; TODO implement all of the freaking edit types from the FOX5 spec
+
 (defun gif-fox5 (input-filename output-filename)
   "Loads the GIF file from the provided filename, converts it to a FOX5 file and
 saves the result under the provided filename."
   (let ((gif (skippy:load-data-stream input-filename)))
     ;; TODO warning when over 50 frames
-    (validate-gif gif)
+    ;;(validate-gif gif)
     (multiple-value-bind (images ks) (gif-to-images gif)
       (write-fox5 (gif-make-file images ks) output-filename))))
 
-(defun validate-gif (data-stream)
-  "Validates the image size of the GIF data stream."
-  (assert (= 95 (skippy:width data-stream)))
-  (assert (= 95 (skippy:height data-stream)))
-  (values))
+(defun validate-portrait (data-stream truncatep)
+  "Validate that the provided GIF data stream is a proper portrait."
+  (let ((width (skippy:width data-stream))
+        (height (skippy:height data-stream))
+        (length (length (skippy:images data-stream))))
+    (unless (= 95 width)
+      (warn "Target file has width of ~D pixels; portraits of width different ~
+than 95px will not display correctly." width))
+    (unless (= 95 height)
+      (warn "Target file has height of ~D pixels; portraits of height ~
+different than 95px will not display correctly." height))
+    (unless (>= 50 length)
+      (warn "Target file has ~D animation frames; the maximum for portraits is ~
+50 frames. File will~A be truncated." length (if truncatep " " " NOT")))
+    (values)))
 
 (defun gif-to-images (gif)
   (multiple-value-bind (arrays delays data) (skippy-renderer:render gif)
