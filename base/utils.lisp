e8f76937588d514b6f5273a139d9c5c721440063
Michał "phoe" Herda <phoe@teknik.io>
2018-01-01 14:27:47 +0100

    fix bug in logic

--- base/utils.lisp
+++ base/utils.lisp
@@ -9,6 +9,10 @@
   "Shortcut for FLEXI-STREAMS:STRING-TO-OCTETS."
   (flexi-streams:string-to-octets string :external-format external-format))
 
+(defun deoctetize (vector &optional (external-format :utf-8))
+  "Shortcut for FLEXI-STREAMS:OCTETS-TO-STRING."
+  (flexi-streams:octets-to-string vector :external-format external-format))
+
 (defun read-string (buffer &optional (external-format :utf-8))
   "Reads a FOX5 string from the provided octet buffer. First, it reads a
 U16-BE signifying the number of bytes in the string, and then, it reads that~
@@ -17,4 +21,4 @@ many characters and puts them in the string."
   (let* ((n (readu16-be buffer))
          (vector (make-octet-vector n)))
     (fast-io:fast-read-sequence vector buffer)
-    (octetize vector external-format)))
+    (deoctetize vector external-format)))
